495657c8a1b1396c2c473e8f34926ad4870f1e56
Shiwen <shiwenbot@gmail.com>
2026-08-23 18:22:57 +0800

    新增AI生图任务Mapper

---
+++ mapper/src/main/java/cn/qihangerp/mapper/AiImageTaskMapper.java
@@ -0,0 +1,12 @@
+package cn.qihangerp.mapper;
+
+import cn.qihangerp.model.entity.AiImageTask;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @description 针对表【ai_image_task(AI生图任务表)】的数据库操作Mapper
+ * @Entity cn.qihangerp.model.entity.AiImageTask
+ */
+public interface AiImageTaskMapper extends BaseMapper<AiImageTask> {
+
+}
